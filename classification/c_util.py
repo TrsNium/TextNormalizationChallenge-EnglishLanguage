57708f1c8666e69f024be00ebb211949a7dfb4de
TrsNium <umenyan.mitou@gmail.com>
2017-11-02 02:17:08 +0900

    .

--- classification/c_util.py
+++ classification/c_util.py
@@ -1,4 +1,6 @@
 import os
+import random
+import numpy as np
 
 def char_byte(str_):
     return len(str_.encode("utf-8"))
@@ -58,9 +60,42 @@ def convert_label(labels):
         r.append(content)
     return np.array(r)
 
-def mk_train_func(training_data_path, char_dict_path, class_dict_path, word_length, max_time_step, max_word_length=10, test=True):
+def mk_train_func(training_data_path, class_data_path, char_dict_path, class_dict_path, batch_size, word_length, max_time_step, max_word_length=10, test=True, p=0.7):
+    training_data = read_dict(training_data_path)
+    label_data = read_dict(class_data_path)
     char_dict = read_dict(char_dict_path)
     class_dict = read_dict(class_dict_path)
+    
+    if test:
+        choiced_idx = [idx for idx in range(len(training_data)) if np.random.binomial(n=1, p)]
+        training_data = [training_data[idx] for idx in choiced_idx]
+        label_data = [label_data[idx] for idx in choiced_idx]
+        test_data = [training_data[idx] for idx in range(len(training_data)) if idx not in choiced_idx]
+        test_label_data = [label_data[idx] for idx in range(len(label_data)) if idx not in choiced_idx]
+
+    def train_func():
+        data_size = len(training_data)
+        training_data = np.array(training_data)
+        label_data = np.array(label_data)
+        while True:
+            choiced_idx = np.random.choice(data_size, batch_size)
+            choiced_s = training_data[choiced_idx].tolist()
+
+            
+
+
+            yield
+
+    def test_func():
+        while True:
+
+            yield
+        
+
+    if test:
+        return train_func, test_func
+
+    return train_func
 
 #save_dict(mk_word_dict(["../data/test_before_sentence_.txt", "../data/train_before_sentence_.txt"]), "../data/word_dict.txt")
 #save_dict(mk_charactor_dict(["../data/test_before_sentence_.txt", "../data/train_before_sentence_.txt"]), "../data/char_dict.txt")
